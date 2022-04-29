f58e8eba59ca8408c577996338175338c3aa0f9d
unknown <melissairishgarcia9@gmail.com>
2022-04-29 22:21:01 +0800

    Remove Restaurant

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <meta name="description" content="" />
   <meta name="author" content="" />
   <link rel="shortcut icon" href="images/logo2.png" type="">
-  <title> Piggy Wings Restaurant </title>
+  <title> Piggy Wings </title>
   <!-- bootstrap core css -->
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
   <link rel="stylesheet" type="text/css" href="css/footer.css" />
